d3ea69f6be075154f3fa04bef6604e2b363dd16b
Abhishek Yadav <50040484+abhishekyadav-aps@users.noreply.github.com>
2020-07-12 17:49:38 +0530

    complete

--- app/src/main/java/com/abhitom/pokemonclassifier/MainActivity.kt
+++ app/src/main/java/com/abhitom/pokemonclassifier/MainActivity.kt
@@ -3,41 +3,157 @@ package com.abhitom.pokemonclassifier
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.ByteArrayOutputStream
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.common.TensorOperator
+import org.tensorflow.lite.support.common.TensorProcessor
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
+import org.tensorflow.lite.support.label.TensorLabel
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    val PICK_IMAGE=1
-    var uri:Uri?=null
-    var imgData:Byte?=null
-
+    protected var tflite: Interpreter? = null
+    private val tfliteModel: MappedByteBuffer? = null
+    private var inputImageBuffer: TensorImage? = null
+    private var imageSizeX = 0
+    private var imageSizeY = 0
+    private var outputProbabilityBuffer: TensorBuffer? = null
+    private var probabilityProcessor: TensorProcessor? = null
+    private val IMAGE_MEAN = 0.0f
+    private val IMAGE_STD = 255.0f
+    private val PROBABILITY_MEAN = 0.0f
+    private val PROBABILITY_STD = 255.0f
+    private var bitmap: Bitmap? = null
+    private var labels: List<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ivPokemon.setOnClickListener {
-            var gallery=Intent()
-            gallery.setType("image/*")
-            gallery.setAction(Intent.ACTION_GET_CONTENT)
-            startActivityForResult(Intent.createChooser(gallery,"Select Picture"),PICK_IMAGE)
+        ivPokemon!!.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 12)
+        }
+        try {
+            tflite = Interpreter(this.loadmodelfile(this)!!)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        btnClassify!!.setOnClickListener {
+            val imageTensorIndex = 0
+            val imageShape: IntArray =
+                tflite!!.getInputTensor(imageTensorIndex).shape() // {1, height, width, 3}
+            imageSizeY = imageShape[1]
+            imageSizeX = imageShape[2]
+            val imageDataType: DataType = tflite!!.getInputTensor(imageTensorIndex).dataType()
+            val probabilityTensorIndex = 0
+            val probabilityShape: IntArray =
+                tflite!!.getOutputTensor(probabilityTensorIndex).shape() // {1, NUM_CLASSES}
+            val probabilityDataType: DataType =
+                tflite!!.getOutputTensor(probabilityTensorIndex).dataType()
+            inputImageBuffer = TensorImage(imageDataType)
+            outputProbabilityBuffer =
+                TensorBuffer.createFixedSize(probabilityShape, probabilityDataType)
+            probabilityProcessor = TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build()
+            inputImageBuffer = loadImage(bitmap!!)
+            tflite!!.run(
+                inputImageBuffer!!.getBuffer(),
+                outputProbabilityBuffer!!.getBuffer().rewind()
+            )
+            showresult()
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    private fun loadImage(bitmap: Bitmap): TensorImage? {
+        // Loads bitmap into a TensorImage.
+        inputImageBuffer!!.load(bitmap)
+
+        // Creates processor for the TensorImage.
+        val cropSize = Math.min(bitmap!!.width, bitmap.height)
+        val imageProcessor: ImageProcessor = ImageProcessor.Builder()
+            .add(ResizeWithCropOrPadOp(cropSize, cropSize))
+            .add(ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+            .add(getPreprocessNormalizeOp())
+            .build()
+        return imageProcessor.process(inputImageBuffer)
+    }
+
+    @Throws(IOException::class)
+    private fun loadmodelfile(activity: Activity): MappedByteBuffer? {
+        val fileDescriptor =
+            activity.assets.openFd("converted_model.tflite")
+        val inputStream =
+            FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel = inputStream.channel
+        val startoffset = fileDescriptor.startOffset
+        val declaredLength = fileDescriptor.declaredLength
+        return fileChannel.map(
+            FileChannel.MapMode.READ_ONLY,
+            startoffset,
+            declaredLength
+        )
+    }
+
+    private fun getPreprocessNormalizeOp(): TensorOperator? {
+        return NormalizeOp(IMAGE_MEAN, IMAGE_STD)
+    }
+
+    private fun getPostprocessNormalizeOp(): TensorOperator? {
+        return NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD)
+    }
+
+    private fun showresult() {
+        try {
+            labels = FileUtil.loadLabels(this, "output.txt")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val labeledProbability: Map<String, Float> =
+            TensorLabel(labels!!.toList(), probabilityProcessor!!.process(outputProbabilityBuffer))
+                .mapWithFloatValue
+        val maxValueInMap =
+            Collections.max(labeledProbability.values)
+        for ((key, value) in labeledProbability) {
+            Log.i("probabilty",key+" -> "+value)
+            if (value == maxValueInMap) {
+                if (value>0.003)
+                    tvPokemonName.text = key
+                else
+                    tvPokemonName.text="Can Not Classify"
+            }
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode==PICK_IMAGE && resultCode== Activity.RESULT_OK){
-            uri=data?.data
-            var bitmap=MediaStore.Images.Media.getBitmap(contentResolver,uri)
-            ivPokemon.setImageBitmap(bitmap)
+        if (requestCode == 12 && resultCode == Activity.RESULT_OK && data != null) {
+            var imageuri = data.data
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageuri)
+                ivPokemon!!.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
-    }
-
-    private fun convertBitmapToByteBuffer(bitmap: Bitmap?):ByteArray {
-        val stream = ByteArrayOutputStream()
-        bitmap?.compress(Bitmap.CompressFormat.PNG, 100, stream)
-        return stream.toByteArray()
     }
 }
+
